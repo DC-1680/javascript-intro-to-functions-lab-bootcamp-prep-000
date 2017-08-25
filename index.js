d42ecbdbe6a2baa0e8e5906631dbcf6cc829fdea
Daryl Concha <concha.daryl@gmail.com>
2017-08-25 17:53:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-var spy = 'hello'
 function logShout(spy) {
   console.log (spy.toUpperCase)
 }
 
 function logWhisper(string) {
   console.log (string.toLowerCase)
+}
+
+var uppercase = 'HELLO!'
+var lowercase = 'hello!'
+var mixedCase = 'Hi there!'
+
+function sayHiToGrandma(string) {
+  if (sayHiToGrandma = lowercase) {
+    console.log("I can't hear you!")
 }
